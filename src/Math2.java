6fa138c52fe17510da8d1905571bac9d3c810520
Macaya Jones <macaya.jones32@gmail.com>
2017-10-18 00:33:08 -0400

    first commit

---
+++ src/Math2.java
@@ -0,0 +1,12 @@
+public class Math2 {
+    public static void main(String[] args) {
+        double myothernumber;
+        int mynumber;
+
+        myothernumber = 1.7938;
+        mynumber = 2;
+
+        System.out.println("My number is " + mynumber);
+        System.out.println("My other number is " + myothernumber);
+    }
+}
